a396ccec25e3d6f734357a9a4b10634331733938
umitems <umitems@gmail.com>
2015-08-24 17:33:46 +0700

    add floating + btn

--- app/src/main/java/com/vi8e/um/wunderlist/MainActivity.java
+++ app/src/main/java/com/vi8e/um/wunderlist/MainActivity.java
@@ -5,67 +5,54 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 
 public
 class MainActivity extends AppCompatActivity {
 
 
-Toolbar                 toolbar;
+Toolbar toolbar;
 CollapsingToolbarLayout collapsingToolbarLayout;
 
-CoordinatorLayout    rootLayout;
+DrawerLayout drawerLayout;
+ActionBarDrawerToggle drawerToggle;
+
+CoordinatorLayout rootLayout;
 FloatingActionButton fabBtn;
 
 @Override
-protected
-void onCreate ( Bundle savedInstanceState ) {
-	super.onCreate ( savedInstanceState );
-	setContentView ( R.layout.activity_main );
-
-	initToolbar ();
-	initInstances ();
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	Fabric.with(this, new Crashlytics());
+	setContentView(R.layout.activity_main);
+	initToolbar();
+	initInstances();
 }
 
-private
-void initToolbar () {
-	toolbar = ( Toolbar ) findViewById ( R.id.toolbar );
-	setSupportActionBar ( toolbar );
+private void initToolbar() {
+	//toolbar = (Toolbar) findViewById(R.id.toolbar);
+//	setSupportActionBar(toolbar);
 }
 
-private
-void initInstances () {
-	drawerLayout = ( DrawerLayout ) findViewById ( R.id.drawerLayout );
-	drawerToggle = new ActionBarDrawerToggle ( MainActivity.this, drawerLayout, R.string.hello_world, R.string.hello_world );
-	drawerLayout.setDrawerListener ( drawerToggle );
+private void initInstances() {
+	drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+	drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.hello_world, R.string.hello_world);
+	drawerLayout.setDrawerListener(drawerToggle);
 
-	getSupportActionBar ().setHomeButtonEnabled ( true );
-	getSupportActionBar ().setDisplayHomeAsUpEnabled ( true );
+	getSupportActionBar().setHomeButtonEnabled(true);
+	getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-	rootLayout = ( CoordinatorLayout) findViewById(R.id.rootLayout);
-
-	fabBtn = (FloatingActionButton) findViewById(R.id.fabBtn);
-	fabBtn.setOnClickListener(new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			Snackbar.make ( rootLayout, "Hello. I am Snackbar!", Snackbar.LENGTH_SHORT )
-							.setAction("Undo", new View.OnClickListener() {
-								@Override
-								public void onClick(View v) {
-
-								}
-							})
-							.show();
-		}
-	});
+	rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
 
 	collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
 	collapsingToolbarLayout.setTitle("Design Library");
@@ -73,12 +60,14 @@ void initInstances () {
 
 @Override
 public void onPostCreate(Bundle savedInstanceState) {
-	super.onPostCreate ( savedInstanceState );
+	super.onPostCreate(savedInstanceState);
+	drawerToggle.syncState ();
 }
 
 @Override
 public void onConfigurationChanged(Configuration newConfig) {
-	super.onConfigurationChanged ( newConfig );
+	super.onConfigurationChanged(newConfig);
+	drawerToggle.onConfigurationChanged ( newConfig );
 }
 
 @Override
@@ -90,6 +79,9 @@ public boolean onCreateOptionsMenu(Menu menu) {
 
 @Override
 public boolean onOptionsItemSelected(MenuItem item) {
+	if (drawerToggle.onOptionsItemSelected(item))
+		return true;
+
 	// Handle action bar item clicks here. The action bar will
 	// automatically handle clicks on the Home/Up button, so long
 	// as you specify a parent activity in AndroidManifest.xml.
