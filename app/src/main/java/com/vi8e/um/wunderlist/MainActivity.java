82a4d5096e4141a984ec5b143b4b9317acba4361
umitems <umitems@gmail.com>
2015-08-24 14:11:39 +0700

    integrated libs  compile 'com.android.support:recyclerview-v7:22.2.1'                     compile 'com.android.support:cardview-v7:22.2.1'                     compile 'com.android.support:palette-v7:22.2.1'                     compile 'com.android.support:design:22.2.1'                     compile 'com.squareup.picasso:picasso:2.4.0'                     compile 'com.melnykov:floatingactionbutton:1.3.0'

--- app/src/main/java/com/vi8e/um/wunderlist/MainActivity.java
+++ app/src/main/java/com/vi8e/um/wunderlist/MainActivity.java
@@ -1,42 +1,70 @@
 package com.vi8e.um.wunderlist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public
 class MainActivity extends AppCompatActivity {
 
 @Override
-protected
-void onCreate ( Bundle savedInstanceState ) {
-	super.onCreate ( savedInstanceState );
-	setContentView ( R.layout.activity_main );
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	//setContentView(R.layout.activity_my);
+
+	setContentView(R.layout.activity_my);
+	RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
+	recList.setHasFixedSize(true);
+	LinearLayoutManager llm = new LinearLayoutManager(this);
+	llm.setOrientation(LinearLayoutManager.VERTICAL);
+	recList.setLayoutManager(llm);
+
+	ContactAdapter ca = new ContactAdapter(createList(30));
+	recList.setAdapter(ca);
 }
 
+
 @Override
-public
-boolean onCreateOptionsMenu ( Menu menu ) {
+public boolean onCreateOptionsMenu(Menu menu) {
 	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater ().inflate ( R.menu.menu_main, menu );
+	getMenuInflater().inflate(R.menu.my, menu);
 	return true;
 }
 
 @Override
-public
-boolean onOptionsItemSelected ( MenuItem item ) {
+public boolean onOptionsItemSelected(MenuItem item) {
 	// Handle action bar item clicks here. The action bar will
 	// automatically handle clicks on the Home/Up button, so long
 	// as you specify a parent activity in AndroidManifest.xml.
-	int id = item.getItemId ();
-
-	//noinspection SimplifiableIfStatement
-	if ( id == R.id.action_settings ) {
+	int id = item.getItemId();
+	if (id == R.id.action_settings) {
 		return true;
 	}
+	return super.onOptionsItemSelected(item);
+}
 
-	return super.onOptionsItemSelected ( item );
+
+
+private List<ContactInfo> createList(int size) {
+
+	List<ContactInfo> result = new ArrayList<ContactInfo> ();
+	for (int i=1; i <= size; i++) {
+		ContactInfo ci = new ContactInfo();
+		ci.name = ContactInfo.NAME_PREFIX + i;
+		ci.surname = ContactInfo.SURNAME_PREFIX + i;
+		ci.email = ContactInfo.EMAIL_PREFIX + i + "@test.com";
+
+		result.add(ci);
+
+	}
+
+	return result;
 }
 }
