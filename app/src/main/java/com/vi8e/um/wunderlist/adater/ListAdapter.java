7d8b8fb461291e837ee7098fdd9e27e7751c4d5c
umitems <umitems@gmail.com>
2015-08-26 18:37:49 +0700

    adding onClick to star in ListActivity toggle img

--- app/src/main/java/com/vi8e/um/wunderlist/adater/ListAdapter.java
+++ app/src/main/java/com/vi8e/um/wunderlist/adater/ListAdapter.java
@@ -1,7 +1,6 @@
 package com.vi8e.um.wunderlist.adater;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,14 +23,19 @@ import java.util.ArrayList;
  */
 public
 class ListAdapter extends ArrayAdapter<ListModel> {
+
+Context mContext;
+
 public
 ListAdapter ( Context context, ArrayList<ListModel> users ) {
+
 	super ( context, 0, users );
+	mContext = context;
 }
 
 @Override
 public
-View getView ( int position, View convertView, ViewGroup parent ) {
+View getView ( int position, View convertView, final ViewGroup parent ) {
 	// Get the data item for this position
 	ListModel listModel = getItem ( position );
 	// Check if an existing view is being reused, otherwise inflate the view
@@ -42,22 +46,23 @@ View getView ( int position, View convertView, ViewGroup parent ) {
 	final TextView tvTitle = ( TextView ) convertView.findViewById ( R.id.listtitle );
 	TextView tvLateTask = ( TextView ) convertView.findViewById ( R.id.latetask );
 	TextView tvCurrentTask = ( TextView ) convertView.findViewById ( R.id.currentTask );
-	final ImageView star = (ImageView)convertView.findViewById ( R.id.star );
+	final ImageView star = ( ImageView ) convertView.findViewById ( R.id.star );
 	// Populate the data into the template view using the data object
 	tvTitle.setText ( listModel.getListTitle () );
 	tvCurrentTask.setText ( String.valueOf ( listModel.getNumCurrentTask () ) );
 	tvLateTask.setText ( String.valueOf ( listModel.getNumLateTask () ) );
 
-
 	star.setOnClickListener ( new View.OnClickListener () {
 		@Override public
 		void onClick ( View v ) {
-			if ( star.getId () == 1 ) {
-				star.setBackgroundColor ( Color.RED );
-				star.setId ( 0 );
+			if ( star.getId () == 0 ) {
+
+				star.setBackground ( mContext.getResources ().getDrawable ( R.mipmap.wl_task_detail_ribbon ) );
+				star.setId ( 1 );
 			}
 			else {
-				star.setId ( 1 );
+				star.setBackground ( mContext.getResources ().getDrawable ( R.mipmap.wl_task_detail_ribbon_selected ) );
+				star.setId ( 0 );
 			}
 		}
 	} );
@@ -66,11 +71,9 @@ View getView ( int position, View convertView, ViewGroup parent ) {
 	convertView.setOnClickListener ( new View.OnClickListener () {
 		@Override public
 		void onClick ( View v ) {
-			Intent  intent = new Intent ( getContext (), ListActivity.class );
+			Intent intent = new Intent ( getContext (), ListActivity.class );
 			intent.putExtra ( ListConst.KEY_TITLE, tvTitle.getText ().toString () );
 			getContext ().startActivity ( intent );
-
-
 
 		}
 	} );
