8d2b970017ec1756ab96a0f662b1aa697b382d27
umitems <umitems@gmail.com>
2015-09-02 19:25:45 +0700

    cleanCode

--- app/src/main/java/com/vi8e/um/wunderlist/adater/LandingListAdapter.java
+++ app/src/main/java/com/vi8e/um/wunderlist/adater/LandingListAdapter.java
@@ -1,6 +1,7 @@
 package com.vi8e.um.wunderlist.adater;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -38,38 +39,51 @@ public
 ArrayList<ListModel> getArrayList () {
 	return lists;
 }
+ListModel listModel;
 
 @Override
 public
 View getView ( final int position, View convertView, ViewGroup parent ) {
 
-	final ListModel listModel = getItem ( position );
+	listModel = getItem ( position );
 	// Check if an existing view is being reused, otherwise inflate the view
 	if ( convertView == null ) {
 		convertView = LayoutInflater.from ( getContext () ).inflate ( R.layout.list_row_landing, parent, false );
 	}
-	// Lookup view for data population
+
 	final TextView tvTitle = ( TextView ) convertView.findViewById ( R.id.listtitle );
 	TextView tvLateTask = ( TextView ) convertView.findViewById ( R.id.latetask );
 	TextView tvCurrentTask = ( TextView ) convertView.findViewById ( R.id.currentTask );
 	final ListView listView = ( ListView ) convertView.getParent ();
 
-	// Populate the data into the template view using the data object
 	tvTitle.setText ( listModel.getListTitle () );
 	tvCurrentTask.setText ( String.valueOf ( listModel.getNumCurrentTask () ) );
 	tvLateTask.setText ( String.valueOf ( listModel.getNumLateTask () ) );
 
-	convertView.setOnClickListener ( new View.OnClickListener () {
+	convertView.setOnClickListener ( getOnClick ( tvTitle,getContext () ) );
+	convertView.setOnLongClickListener ( getOnLongClick () );
+
+	// Return the completed view to render on screen
+	return convertView;
+}
+
+@NonNull public static
+View.OnClickListener getOnClick ( final TextView tvTitle, final Context context ) {
+	return new View.OnClickListener () {
 		@Override public
 		void onClick ( View v ) {
-//			Intent  intent = new Intent ( getContext (), TaskActivity.class );
-			Intent intent = new Intent ( getContext (), DeveloperActivity.class );
+			//Intent  intent = new Intent ( getContext (), TaskActivity.class );
+			Intent intent = new Intent (context, DeveloperActivity.class );
 			intent.putExtra ( ListConst.KEY_TITLE, tvTitle.getText ().toString () );
-			getContext ().startActivity ( intent );
+			context.startActivity ( intent );
 
 		}
-	} );
-	convertView.setOnLongClickListener ( new View.OnLongClickListener () {
+	};
+}
+
+@NonNull private
+View.OnLongClickListener getOnLongClick () {
+	return new View.OnLongClickListener () {
 		@Override public
 		boolean onLongClick ( View v ) {
 
@@ -81,10 +95,7 @@ View getView ( final int position, View convertView, ViewGroup parent ) {
 			where.delete ( context );
 			return false;
 		}
-	} );
-
-	// Return the completed view to render on screen
-	return convertView;
+	};
 }
 
 
