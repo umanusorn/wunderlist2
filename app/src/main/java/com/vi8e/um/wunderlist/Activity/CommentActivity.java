d5cc9b8689345d1ad09731aca823021adea6a5f1
umitems <umitems@gmail.com>
2015-10-06 15:29:11 +0700

    update comment ui

--- app/src/main/java/com/vi8e/um/wunderlist/Activity/CommentActivity.java
+++ app/src/main/java/com/vi8e/um/wunderlist/Activity/CommentActivity.java
@@ -5,19 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.vi8e.um.wunderlist.R;
+import com.vi8e.um.wunderlist.utils.ActivityUi;
 
 
 public
 class CommentActivity extends AppCompatActivity {
-
+Toolbar mToolbar;
+String title ="Comment";
 @Override
 protected
 void onCreate ( Bundle savedInstanceState ) {
 	super.onCreate ( savedInstanceState );
 	setContentView ( R.layout.activity_comment );
-	Toolbar toolbar = ( Toolbar ) findViewById ( R.id.toolbar );
-	setSupportActionBar ( toolbar );
-
+	mToolbar = ( Toolbar ) findViewById ( R.id.toolbar );
+	setSupportActionBar ( mToolbar );
+	ActivityUi.setToolBar ( this, mToolbar, title );
+	mToolbar.setBackgroundColor ( getResources ().getColor( R.color.blue_500 ) );
 }
+
 
 }
