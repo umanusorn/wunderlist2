8706e2ed41ef13397e3987594919fea0b3f08c5d
umitems <umitems@gmail.com>
2015-10-14 10:23:32 +0700

    clean code

--- app/src/main/java/com/vi8e/um/wunderlist/Activity/LoginDropboxActivity.java
+++ app/src/main/java/com/vi8e/um/wunderlist/Activity/LoginDropboxActivity.java
@@ -56,7 +56,8 @@ import java.util.Date;
 import java.util.Locale;
 
 
-public class LoginDropboxActivity extends Activity {
+public
+class LoginDropboxActivity extends Activity {
 private static final String TAG = "DBRoulette";
 
 ///////////////////////////////////////////////////////////////////////////
@@ -86,7 +87,7 @@ DropboxAPI<AndroidAuthSession> mApi;
 private boolean mLoggedIn;
 
 // Android widgets
-private Button       mSubmit;
+private Button mSubmit;
 
 private final String PHOTO_DIR = "/Photos/";
 
@@ -117,10 +118,10 @@ void onCreate ( Bundle savedInstanceState ) {
 		}
 	} );
 
-        // Display the proper UI state if logged in or not
-        setLoggedIn(mApi.getSession().isLinked());
+	// Display the proper UI state if logged in or not
+	setLoggedIn ( mApi.getSession ().isLinked () );
 
-    }
+}
 
 public
 void toggleLoginDropbox () {
@@ -153,189 +154,206 @@ void uploadViaCamera () {
 	DateFormat df = new SimpleDateFormat ( "yyyy-MM-dd-kk-mm-ss", Locale.US );
 
 	String newPicFile = df.format ( date ) + ".jpg";
-	String outPath = new File ( Environment.getExternalStorageDirectory (), newPicFile).getPath();
-	File outFile = new File(outPath);
+	String outPath = new File ( Environment.getExternalStorageDirectory (), newPicFile ).getPath ();
+	File outFile = new File ( outPath );
 
-	mCameraFileName = outFile.toString();
-	Uri outuri = Uri.fromFile(outFile);
-	intent.putExtra(MediaStore.EXTRA_OUTPUT, outuri);
+	mCameraFileName = outFile.toString ();
+	Uri outuri = Uri.fromFile ( outFile );
+	intent.putExtra ( MediaStore.EXTRA_OUTPUT, outuri );
 	Log.i ( TAG, "Importing New Picture: " + mCameraFileName );
 	try {
-	    startActivityForResult(intent, NEW_PICTURE);
-	} catch (ActivityNotFoundException e) {
-	    showToast("There doesn't seem to be a camera.");
+		startActivityForResult ( intent, NEW_PICTURE );
+	}
+	catch ( ActivityNotFoundException e ) {
+		showToast ( "There doesn't seem to be a camera." );
 	}
 }
 
 @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putString("mCameraFileName", mCameraFileName);
-        super.onSaveInstanceState(outState);
-    }
+protected
+void onSaveInstanceState ( Bundle outState ) {
+	outState.putString ( "mCameraFileName", mCameraFileName );
+	super.onSaveInstanceState ( outState );
+}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        AndroidAuthSession session = mApi.getSession();
+@Override
+protected
+void onResume () {
+	super.onResume ();
+	AndroidAuthSession session = mApi.getSession ();
 
-        // The next part must be inserted in the onResume() method of the
-        // activity from which session.startAuthentication() was called, so
-        // that Dropbox authentication completes properly.
-        if (session.authenticationSuccessful()) {
-            try {
-                // Mandatory call to complete the auth
-                session.finishAuthentication();
+	// The next part must be inserted in the onResume() method of the
+	// activity from which session.startAuthentication() was called, so
+	// that Dropbox authentication completes properly.
+	if ( session.authenticationSuccessful () ) {
+		try {
+			// Mandatory call to complete the auth
+			session.finishAuthentication ();
 
-                // Store it locally in our app for later use
-                storeAuth(session);
-                setLoggedIn(true);
-            } catch (IllegalStateException e) {
-                showToast("Couldn't authenticate with Dropbox:" + e.getLocalizedMessage());
-                Log.i(TAG, "Error authenticating", e);
-            }
-        }
-    }
+			// Store it locally in our app for later use
+			storeAuth ( session );
+			setLoggedIn ( true );
+		}
+		catch ( IllegalStateException e ) {
+			showToast ( "Couldn't authenticate with Dropbox:" + e.getLocalizedMessage () );
+			Log.i ( TAG, "Error authenticating", e );
+		}
+	}
+}
 
-    // This is what gets called on finishing a media piece to import
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == NEW_PICTURE) {
-            // return from file upload
-            if (resultCode == Activity.RESULT_OK) {
-                Uri uri = null;
-                if (data != null) {
-                    uri = data.getData();
-                }
-                if (uri == null && mCameraFileName != null) {
-                    uri = Uri.fromFile(new File(mCameraFileName));
-                }
-                File file = new File(mCameraFileName);
-								Log.d ( TAG,"before upload" );
-                if (uri != null) {
-                    UploadPicture upload = new UploadPicture(this, mApi, PHOTO_DIR, file);
-                    upload.execute();
-                }
-            } else {
-                Log.w(TAG, "Unknown Activity Result from mediaImport: "
-                        + resultCode);
-            }
-        }
-    }
+// This is what gets called on finishing a media piece to import
+@Override
+public
+void onActivityResult ( int requestCode, int resultCode, Intent data ) {
+	if ( requestCode == NEW_PICTURE ) {
+		// return from file upload
+		if ( resultCode == Activity.RESULT_OK ) {
+			Uri uri = null;
+			if ( data != null ) {
+				uri = data.getData ();
+			}
+			if ( uri == null && mCameraFileName != null ) {
+				uri = Uri.fromFile ( new File ( mCameraFileName ) );
+			}
+			File file = new File ( mCameraFileName );
+			Log.d ( TAG, "before upload" );
+			if ( uri != null ) {
+				UploadPicture upload = new UploadPicture ( this, mApi, PHOTO_DIR, file );
+				upload.execute ();
+			}
+		}
+		else {
+			Log.w ( TAG, "Unknown Activity Result from mediaImport: "
+			             + resultCode );
+		}
+	}
+}
 
-    private void logOut() {
-        // Remove credentials from the session
-        mApi.getSession().unlink();
+private
+void logOut () {
+	// Remove credentials from the session
+	mApi.getSession ().unlink ();
 
-        // Clear our stored keys
-        clearKeys();
-        // Change UI state to display logged out version
-        setLoggedIn(false);
-    }
+	// Clear our stored keys
+	clearKeys ();
+	// Change UI state to display logged out version
+	setLoggedIn ( false );
+}
 
-    /**
-     * Convenience function to change UI state based on being logged in
-     */
-    private void setLoggedIn(boolean loggedIn) {
-    	mLoggedIn = loggedIn;
-    	if (loggedIn) {
-    		mSubmit.setText ( "Unlink from Dropbox" );
-            //mDisplay.setVisibility(View.VISIBLE);
-    	} else {
-    		mSubmit.setText("Link with Dropbox");
-    	}
-    }
+/**
+ * Convenience function to change UI state based on being logged in
+ */
+private
+void setLoggedIn ( boolean loggedIn ) {
+	mLoggedIn = loggedIn;
+	if ( loggedIn ) {
+		mSubmit.setText ( "Unlink from Dropbox" );
+		//mDisplay.setVisibility(View.VISIBLE);
+	}
+	else {
+		mSubmit.setText ( "Link with Dropbox" );
+	}
+}
 
-    private void checkAppKeySetup() {
-        // Check to make sure that we have a valid app key
-        if (APP_KEY.startsWith("CHANGE") ||
-                APP_SECRET.startsWith("CHANGE")) {
-            showToast("You must apply for an app key and secret from developers.dropbox.com, and add them to the DBRoulette ap before trying it.");
-            finish();
-            return;
-        }
+private
+void checkAppKeySetup () {
+	// Check to make sure that we have a valid app key
+	if ( APP_KEY.startsWith ( "CHANGE" ) ||
+	     APP_SECRET.startsWith ( "CHANGE" ) ) {
+		showToast ( "You must apply for an app key and secret from developers.dropbox.com, and add them to the DBRoulette ap before trying it." );
+		finish ();
+		return;
+	}
 
-        // Check if the app has set up its manifest properly.
-        Intent testIntent = new Intent(Intent.ACTION_VIEW);
-        String scheme = "db-" + APP_KEY;
-        String uri = scheme + "://" + AuthActivity.AUTH_VERSION + "/test";
-        testIntent.setData(Uri.parse(uri));
-        PackageManager pm = getPackageManager();
-        if (0 == pm.queryIntentActivities(testIntent, 0).size()) {
-            showToast("URL scheme in your app's " +
-                    "manifest is not set up correctly. You should have a " +
-                    "com.dropbox.client2.android.AuthActivity with the " +
-                    "scheme: " + scheme);
-            finish();
-        }
-    }
+	// Check if the app has set up its manifest properly.
+	Intent testIntent = new Intent ( Intent.ACTION_VIEW );
+	String scheme = "db-" + APP_KEY;
+	String uri = scheme + "://" + AuthActivity.AUTH_VERSION + "/test";
+	testIntent.setData ( Uri.parse ( uri ) );
+	PackageManager pm = getPackageManager ();
+	if ( 0 == pm.queryIntentActivities ( testIntent, 0 ).size () ) {
+		showToast ( "URL scheme in your app's " +
+		            "manifest is not set up correctly. You should have a " +
+		            "com.dropbox.client2.android.AuthActivity with the " +
+		            "scheme: " + scheme );
+		finish ();
+	}
+}
 
-    private void showToast(String msg) {
-        Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
-        error.show();
-    }
+private
+void showToast ( String msg ) {
+	Toast error = Toast.makeText ( this, msg, Toast.LENGTH_LONG );
+	error.show ();
+}
 
-    /**
-     * Shows keeping the access keys returned from Trusted Authenticator in a local
-     * store, rather than storing user name & password, and re-authenticating each
-     * time (which is not to be done, ever).
-     */
-    private void loadAuth(AndroidAuthSession session) {
-        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
-        String key = prefs.getString(ACCESS_KEY_NAME, null);
-        String secret = prefs.getString(ACCESS_SECRET_NAME, null);
-        if (key == null || secret == null || key.length() == 0 || secret.length() == 0) return;
+/**
+ * Shows keeping the access keys returned from Trusted Authenticator in a local
+ * store, rather than storing user name & password, and re-authenticating each
+ * time (which is not to be done, ever).
+ */
+private
+void loadAuth ( AndroidAuthSession session ) {
+	SharedPreferences prefs = getSharedPreferences ( ACCOUNT_PREFS_NAME, 0 );
+	String key = prefs.getString ( ACCESS_KEY_NAME, null );
+	String secret = prefs.getString ( ACCESS_SECRET_NAME, null );
+	if ( key == null || secret == null || key.length () == 0 || secret.length () == 0 ) {
+		return;
+	}
 
-        if (key.equals("oauth2:")) {
-            // If the key is set to "oauth2:", then we can assume the token is for OAuth 2.
-            session.setOAuth2AccessToken(secret);
-        } else {
-            // Still support using old OAuth 1 tokens.
-            session.setAccessTokenPair(new AccessTokenPair (key, secret));
-        }
-    }
+	if ( key.equals ( "oauth2:" ) ) {
+		// If the key is set to "oauth2:", then we can assume the token is for OAuth 2.
+		session.setOAuth2AccessToken ( secret );
+	}
+	else {
+		// Still support using old OAuth 1 tokens.
+		session.setAccessTokenPair ( new AccessTokenPair ( key, secret ) );
+	}
+}
 
-    /**
-     * Shows keeping the access keys returned from Trusted Authenticator in a local
-     * store, rather than storing user name & password, and re-authenticating each
-     * time (which is not to be done, ever).
-     */
-    private void storeAuth(AndroidAuthSession session) {
-        // Store the OAuth 2 access token, if there is one.
-        String oauth2AccessToken = session.getOAuth2AccessToken();
-        if (oauth2AccessToken != null) {
-            SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
-            Editor edit = prefs.edit();
-            edit.putString(ACCESS_KEY_NAME, "oauth2:");
-            edit.putString(ACCESS_SECRET_NAME, oauth2AccessToken);
-            edit.commit();
-            return;
-        }
-        // Store the OAuth 1 access token, if there is one.  This is only necessary if
-        // you're still using OAuth 1.
-        AccessTokenPair oauth1AccessToken = session.getAccessTokenPair();
-        if (oauth1AccessToken != null) {
-            SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
-            Editor edit = prefs.edit();
-            edit.putString(ACCESS_KEY_NAME, oauth1AccessToken.key);
-            edit.putString(ACCESS_SECRET_NAME, oauth1AccessToken.secret);
-            edit.commit();
-            return;
-        }
-    }
+/**
+ * Shows keeping the access keys returned from Trusted Authenticator in a local
+ * store, rather than storing user name & password, and re-authenticating each
+ * time (which is not to be done, ever).
+ */
+private
+void storeAuth ( AndroidAuthSession session ) {
+	// Store the OAuth 2 access token, if there is one.
+	String oauth2AccessToken = session.getOAuth2AccessToken ();
+	if ( oauth2AccessToken != null ) {
+		SharedPreferences prefs = getSharedPreferences ( ACCOUNT_PREFS_NAME, 0 );
+		Editor edit = prefs.edit ();
+		edit.putString ( ACCESS_KEY_NAME, "oauth2:" );
+		edit.putString ( ACCESS_SECRET_NAME, oauth2AccessToken );
+		edit.commit ();
+		return;
+	}
+	// Store the OAuth 1 access token, if there is one.  This is only necessary if
+	// you're still using OAuth 1.
+	AccessTokenPair oauth1AccessToken = session.getAccessTokenPair ();
+	if ( oauth1AccessToken != null ) {
+		SharedPreferences prefs = getSharedPreferences ( ACCOUNT_PREFS_NAME, 0 );
+		Editor edit = prefs.edit ();
+		edit.putString ( ACCESS_KEY_NAME, oauth1AccessToken.key );
+		edit.putString ( ACCESS_SECRET_NAME, oauth1AccessToken.secret );
+		edit.commit ();
+		return;
+	}
+}
 
-    private void clearKeys() {
-        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
-        Editor edit = prefs.edit();
-        edit.clear();
-        edit.commit();
-    }
+private
+void clearKeys () {
+	SharedPreferences prefs = getSharedPreferences ( ACCOUNT_PREFS_NAME, 0 );
+	Editor edit = prefs.edit ();
+	edit.clear ();
+	edit.commit ();
+}
 
-    private
-    AndroidAuthSession buildSession() {
-        AppKeyPair appKeyPair = new AppKeyPair (APP_KEY, APP_SECRET);
+private
+AndroidAuthSession buildSession () {
+	AppKeyPair appKeyPair = new AppKeyPair ( APP_KEY, APP_SECRET );
 
-        AndroidAuthSession session = new AndroidAuthSession (appKeyPair);
-        loadAuth(session);
-        return session;
-    }
+	AndroidAuthSession session = new AndroidAuthSession ( appKeyPair );
+	loadAuth ( session );
+	return session;
+}
 }
