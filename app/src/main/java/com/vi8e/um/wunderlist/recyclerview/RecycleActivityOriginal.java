ac3cfe7e6a104da90e906aead28bf60bee7acb5f
umitems <umitems@gmail.com>
2015-11-03 17:44:05 +0700

    replace layout , listView with recycleView

--- app/src/main/java/com/vi8e/um/wunderlist/recyclerview/RecycleActivityOriginal.java
+++ app/src/main/java/com/vi8e/um/wunderlist/recyclerview/RecycleActivityOriginal.java
@@ -45,7 +45,7 @@ public class RecycleActivityOriginal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_landing_recycle);
 
         if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
